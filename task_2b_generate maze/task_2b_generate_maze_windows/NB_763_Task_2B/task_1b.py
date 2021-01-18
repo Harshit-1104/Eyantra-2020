9f9291d135f24d961cd1519008457409e95e2899
Harshit-1104 <51269325+Harshit-1104@users.noreply.github.com>
2021-01-18 20:56:56 +0530

    Added task 2

---
+++ task_2b_generate maze/task_2b_generate_maze_windows/NB_763_Task_2B/task_1b.py
@@ -0,0 +1,321 @@
+'''
+*******************************
+*
+*        		===============================================
+*           		Nirikshak Bot (NB) Theme (eYRC 2020-21)
+*        		===============================================
+*
+*  This script is to implement Task 1B of Nirikshak Bot (NB) Theme (eYRC 2020-21).
+*
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or
+*  breach of the terms of this agreement.
+*
+*  e-Yantra - An MHRD project under National Mission on Education using ICT (NMEICT)
+*
+*******************************
+'''
+
+# Team ID:			[ Team-ID ]
+# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Filename:			task_1b.py
+# Functions:		applyPerspectiveTransform, detectMaze, writeToCsv
+# 					[ Comma separated list of functions in this file ]
+# Global variables:
+# 					[ List of global variables defined in this file ]
+
+
+####################### IMPORT MODULES #######################
+## You are not allowed to make any changes in this section. ##
+## You have to implement this task with the three available ##
+## modules for this task (numpy, opencv, csv)               ##
+##############################################################
+import numpy as np
+import cv2
+import csv
+##############################################################
+
+
+################# ADD UTILITY FUNCTIONS HERE #################
+## You can define any utility functions for your code.      ##
+## Please add proper comments to ensure that your code is   ##
+## readable and easy to understand.                         ##
+##############################################################
+
+def orderpoints(pts):
+	rect = np.zeros((4, 2), dtype="float32")
+	s = pts.sum(axis=1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	dif = np.diff(pts, axis=1)
+	rect[1] = pts[np.argmin(dif)]
+	rect[3] = pts[np.argmax(dif)]
+	return rect
+
+
+def four_point_transform(image, pts):
+	rect = orderpoints(pts)
+	a = 0.1
+	rect[2][1] += a
+	rect[1][1] += a
+
+	(tl, tr, br, bl) = rect
+	maxWidth = 512
+	maxHeight = 512
+	dst = np.array([[0, 0], [maxWidth - 1, 0], [maxWidth - 1, maxHeight - 1], [0, maxHeight - 1]], dtype="float32")
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	return warped
+
+
+def fun(im):
+	val = 0
+
+	gate = [False, False, False, False]
+	for i in range(0, 5, 1):
+		if not gate[0] and im[i, 50] == 0:
+			val = val + 2
+			gate[0] = True
+		if not gate[1] and im[99-i, 50] == 0:
+			val = val + 8
+			gate[1] = True
+		if not gate[2] and im[50, i] == 0:
+			val += 1
+			gate[2] = True
+
+		for j in range(40, 71):
+			if not gate[3] and im[j, 99-i] == 0:
+				val += 4
+				gate[3] = True
+
+	return val
+
+##############################################################
+
+
+def applyPerspectiveTransform(input_img):
+	"""
+	Purpose:
+	---
+	takes a maze test case image as input and applies a Perspective Transfrom on it to isolate the maze
+
+	Input Arguments:
+	---
+	`input_img` :   [ numpy array ]
+		maze image in the form of a numpy array
+
+	Returns:
+	---
+	`warped_img` :  [ numpy array ]
+		resultant warped maze image after applying Perspective Transform
+
+	Example call:
+	---
+	warped_img = applyPerspectiveTransform(input_img)
+	"""
+
+	warped_img = None
+
+	##############	ADD YOUR CODE HERE	##############
+
+	img = input_img
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	edged = cv2.Canny(gray, 75, 200)
+	_, thresh = cv2.threshold(edged, 127, 255, cv2.THRESH_BINARY)
+	cnts, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+	for c in cnts:
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+		if len(approx) == 4:
+			screenCnt = approx
+			break
+	warped = four_point_transform(img, screenCnt.reshape(4, 2))
+	warped_img = warped
+	warped_img = cv2.resize(warped, (1280, 1280))
+	##################################################
+
+	return warped_img
+
+
+def detectMaze(warped_img):
+	"""
+	Purpose:
+	---
+	takes the warped maze image as input and returns the maze encoded in form of a 2D array
+
+	Input Arguments:
+	---
+	`warped_img` :    [ numpy array ]
+		resultant warped maze image after applying Perspective Transform
+
+	Returns:
+	---
+	`maze_array` :    [ nested list of lists ]
+		encoded maze in the form of a 2D array
+
+	Example call:
+	---
+	maze_array = detectMaze(warped_img)
+	"""
+
+	##############	ADD YOUR CODE HERE	##############
+	img = warped_img
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+	img = cv2.resize(img, (1000, 1000))
+	l = []
+	for x in range(0, 1000, 100):
+		l2 = []
+		for y in range(0, 1000, 100):
+			im = img[x:x + 100, y:y + 100]
+			val = fun(im)
+			l2.append(val)
+		l.append(l2)
+
+	maze_array = l
+	##################################################
+
+	return maze_array
+
+
+# NOTE:	YOU ARE NOT ALLOWED TO MAKE ANY CHANGE TO THIS FUNCTION
+def writeToCsv(csv_file_path, maze_array):
+    """
+    Purpose:
+    ---
+    takes the encoded maze array and csv file name as input and writes the encoded maze array to the csv file
+
+    Input Arguments:
+    ---
+    `csv_file_path` :	[ str ]
+        file path with name for csv file to write
+
+    `maze_array` :		[ nested list of lists ]
+        encoded maze in the form of a 2D array
+
+    Example call:
+    ---
+    warped_img = writeToCsv('test_cases/maze00.csv', maze_array)
+    """
+
+    with open(csv_file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(maze_array)
+
+
+# NOTE:	YOU ARE NOT ALLOWED TO MAKE ANY CHANGE TO THIS FUNCTION
+#
+# Function Name:    main
+#        Inputs:    None
+#       Outputs:    None
+#       Purpose:    This part of the code is only for testing your solution. The function first takes 'maze00.jpg'
+# 					as input, applies Perspective Transform by calling applyPerspectiveTransform function,
+# 					encodes the maze input in form of 2D array by calling detectMaze function and writes this data to csv file
+# 					by calling writeToCsv function, it then asks the user whether to repeat the same on all maze images
+# 					present in 'test_cases' folder or not. Write your solution ONLY in the space provided in the above
+# 					applyPerspectiveTransform and detectMaze functions.
+
+if __name__ == "_main_":
+
+    # path directory of images in 'test_cases' folder
+    img_dir_path = 'test_cases/'
+
+    # path to 'maze00.jpg' image file
+    file_num = 0
+    img_file_path = img_dir_path + 'maze0' + str(file_num) + '.jpg'
+
+    print('\n============================================')
+    print('\nFor maze0' + str(file_num) + '.jpg')
+
+    # path for 'maze00.csv' output file
+    csv_file_path = img_dir_path + 'maze0' + str(file_num) + '.csv'
+
+    # read the 'maze00.jpg' image file
+    input_img = cv2.imread(img_file_path)
+
+    # get the resultant warped maze image after applying Perspective Transform
+    warped_img = applyPerspectiveTransform(input_img)
+
+    if type(warped_img) is np.ndarray:
+
+        # get the encoded maze in the form of a 2D array
+        maze_array = detectMaze(warped_img)
+
+        if (type(maze_array) is list) and (len(maze_array) == 10):
+
+            print('\nEncoded Maze Array = %s' % (maze_array))
+            print('\n============================================')
+
+            # writes the encoded maze array to the csv file
+            writeToCsv(csv_file_path, maze_array)
+
+            cv2.imshow('warped_img_0' + str(file_num), warped_img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+        else:
+
+            print('\n[ERROR] maze_array returned by detectMaze function is not complete. Check the function in code.\n')
+            exit()
+
+    else:
+
+        print(
+            '\n[ERROR] applyPerspectiveTransform function is not returning the warped maze image in expected format! Check the function in code.\n')
+        exit()
+
+    choice = input('\nDo you want to run your script on all maze images ? => "y" or "n": ')
+
+    if choice == 'y':
+
+        for file_num in range(1, 10):
+
+            # path to image file
+            img_file_path = img_dir_path + 'maze0' + str(file_num) + '.jpg'
+
+            print('\n============================================')
+            print('\nFor maze0' + str(file_num) + '.jpg')
+
+            # path for csv output file
+            csv_file_path = img_dir_path + 'maze0' + str(file_num) + '.csv'
+
+            # read the image file
+            input_img = cv2.imread(img_file_path)
+
+            # get the resultant warped maze image after applying Perspective Transform
+            warped_img = applyPerspectiveTransform(input_img)
+
+            if type(warped_img) is np.ndarray:
+
+                # get the encoded maze in the form of a 2D array
+                maze_array = detectMaze(warped_img)
+
+                if (type(maze_array) is list) and (len(maze_array) == 10):
+
+                    print('\nEncoded Maze Array = %s' % (maze_array))
+                    print('\n============================================')
+
+                    # writes the encoded maze array to the csv file
+                    writeToCsv(csv_file_path, maze_array)
+
+                    cv2.imshow('warped_img_0' + str(file_num), warped_img)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
+
+                else:
+
+                    print(
+                        '\n[ERROR] maze_array returned by detectMaze function is not complete. Check the function in code.\n')
+                    exit()
+
+            else:
+
+                print(
+                    '\n[ERROR] applyPerspectiveTransform function is not returning the warped maze image in expected format! Check the function in code.\n')
+                exit()
+
+    else:
+
+        print('')
---
+++ task_2b_generate maze/task_2b_generate_maze_windows/NB_763_Task_2B/task_2b_script.lua
@@ -0,0 +1,535 @@
+--[[
+*****************************************************************************************
+*
+*        		===============================================
+*           		Nirikshak Bot (NB) Theme (eYRC 2020-21)
+*        		===============================================
+*
+*  This Lua script is to implement Task 2B of Nirikshak Bot (NB) Theme (eYRC 2020-21).
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*  
+*  e-Yantra - An MHRD project under National Mission on Education using ICT (NMEICT)
+*
+*****************************************************************************************
+]]--
+
+
+--[[
+# Team ID:			[ 763 ]
+# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Filename:			task_2b
+# Functions:        createWall, saveTexture, retrieveTexture, reapplyTexture, receiveData, generateHorizontalWalls, 
+#                   generateVerticalWalls, deleteWalls, createMaze, sysCall_init, sysCall_beforeSimulation
+#                   sysCall_afterSimulation, sysCall_cleanup
+# 					[ Comma separated list of functions in this file ]
+# Global variables:	
+# 					[ List of global variables defined in this file ]
+]]--
+
+--[[
+##################### GLOBAL VARIABLES #######################
+## You can add global variables in this section according   ##
+## to your requirement.                                     ##
+## DO NOT TAMPER WITH THE ALREADY DEFINED GLOBAL VARIABLES. ##
+##############################################################
+]]--
+
+maze_array = {}
+baseHandle = -1       --Do not change or delete this variable
+textureID = -1        --Do not change or delete this variable
+textureData = -1       --Do not change or delete this variable
+wallHeight = 0.065
+--############################################################
+
+--[[
+##################### HELPER FUNCTIONS #######################
+## You can add helper functions in this section according   ##
+## to your requirement.                                     ##
+## DO NOT MODIFY OR CHANGE THE ALREADY DEFINED HELPER       ##
+## FUNCTIONS                                                ##
+##############################################################
+]]--
+
+function wait(seconds)
+    local start = os.time()
+    repeat until os.time() > start + seconds
+end
+
+function decode(value)
+    bin = {}
+    
+    for i = 1,4,1 do
+        table.insert(bin, i, value % 2)
+        value = math.floor(value / 2)
+    end
+    
+    return bin
+end
+
+--[[
+**************************************************************
+	Function Name : createWall()
+    Purpose:
+	---
+	Creates a black-colored wall of dimensions 90cm x 10cm x 10cm
+
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+	wallObjectHandle : number
+    
+    returns the object handle of the created wall
+	
+	Example call:
+	---
+	wallObjectHandle = createWall()
+**************************************************************	
+]]--
+function createWall()
+    wallObjectHandle = sim.createPureShape(0, 26, {0.09, 0.01, 0.1}, 0, nil)
+    sim.setShapeColor(wallObjectHandle, nil, sim.colorcomponent_ambient_diffuse, {0, 0, 0})
+    sim.setObjectSpecialProperty(wallObjectHandle, sim.objectspecialproperty_collidable)
+    sim.setObjectSpecialProperty(wallObjectHandle, sim.objectspecialproperty_measurable)
+    sim.setObjectSpecialProperty(wallObjectHandle, sim.objectspecialproperty_detectable_all)
+    sim.setObjectSpecialProperty(wallObjectHandle, sim.objectspecialproperty_renderable)
+    return wallObjectHandle
+end
+
+
+-- pure shape( 0 - cuboid, 26 - static, smooth and edges visible, size, mass, precision)
+--[[
+**************************************************************
+  YOU ARE NOT ALLOWED TO MODIFY OR CALL THIS HELPER FUNCTION
+**************************************************************
+	Function Name : saveTexture()
+    Purpose:
+	---
+	Reads and initializes the applied texture to Base object
+    and saves it to a file.
+
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+	None
+	
+	Example call:
+	---
+	saveTexture()
+**************************************************************	
+]]--
+function saveTexture()
+    baseHandle = sim.getObjectHandle("Base")
+    textureID = sim.getShapeTextureId(baseHandle)
+    textureData=sim.readTexture(textureID ,0,0,0,0,0)
+    sim.saveImage(textureData, {512,512}, 0, "models/other/base_template.png", -1)
+end
+--[[
+**************************************************************
+  YOU ARE NOT ALLOWED TO MODIFY OR CALL THIS HELPER FUNCTION
+**************************************************************
+	Function Name : retrieveTexture()
+    Purpose:
+	---
+	Loads texture from file.
+
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+	None
+	
+	Example call:
+	---
+	retrieveTexture()
+**************************************************************	
+]]--
+function retrieveTexture()
+    textureData, resolution = sim.loadImage(0, "models/other/base_template.png") 
+end
+
+--[[
+**************************************************************
+  YOU ARE NOT ALLOWED TO MODIFY OR CALL THIS HELPER FUNCTION
+**************************************************************
+	Function Name : reapplyTexture()
+    Purpose:
+	---
+	Re-applies texture to Base object
+
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+	None
+	
+	Example call:
+	---
+    reapplyTexture()
+**************************************************************	
+]]--
+function reapplyTexture()
+    plane, textureID = sim.createTexture("", 0, nil, {1.01, 1.01}, nil, 0, {512, 512})
+    sim.writeTexture(textureID, 0, textureData, 0, 0, 0, 0, 0)
+    sim.setShapeTexture(baseHandle, textureID, sim.texturemap_plane, 0, {1.01, 1.01},nil,nil)
+    sim.removeObject(plane)
+end
+
+--############################################################
+
+--[[
+**************************************************************
+	Function Name : receiveData()
+    Purpose:
+	---
+	Receives data via Remote API. This function is called by 
+    simx.callScriptFunction() in the python code (task_2b.py)
+
+	Input Arguments:
+	---
+	inInts : Table of Ints
+    inFloats : Table of Floats
+    inStrings : Table of Strings
+    inBuffer : string
+	
+	Returns:
+	---
+	inInts : Table of Ints
+    inFloats : Table of Floats
+    inStrings : Table of Strings
+    inBuffer : string
+    
+    These 4 variables represent the data being passed from remote
+    API client(python) to the CoppeliaSim scene
+	
+	Example call:
+	---
+	N/A
+    
+    Hint:
+    ---
+    You might want to study this link to understand simx.callScriptFunction()
+    better 
+    https://www.coppeliarobotics.com/helpFiles/en/remoteApiExtension.htm
+**************************************************************	
+]]--
+function receiveData(inInts,inFloats,inStrings,inBuffer)
+
+    --*******************************************************
+    --               ADD YOUR CODE HERE
+    
+    table.insert(maze_array, inInts)
+    
+    --*******************************************************
+    return inInts, inFloats, inStrings, inBuffer
+end
+
+--[[
+**************************************************************
+	Function Name : generateHorizontalWalls()
+    Purpose:
+	---
+	Generates all the Horizontal Walls in the scene.
+
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+    None
+	
+	Example call:
+	---
+	generateHorizontalWalls()
+**************************************************************	
+]]--
+function generateHorizontalWalls()
+
+    --*******************************************************
+    --               ADD YOUR CODE HERE    
+    
+    for i=-0.45,0.45,0.1
+    do
+     for j=0.5,-0.5,-0.1
+     do
+      wallobjectHandle=createWall()
+      sim.setObjectParent(wallobjectHandle,baseHandle,1)
+      sim.setObjectPosition(wallobjectHandle,sim.handle_parent,{i,j,wallHeight})
+     end 
+    end
+       
+    --*******************************************************
+end
+
+--[[
+**************************************************************
+	Function Name : generateVerticalWalls()
+    Purpose:
+	---
+	Generates all the Vertical Walls in the scene.
+
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+    None
+	
+	Example call:
+	---
+	generateVerticalWalls()
+**************************************************************	
+]]--
+
+function generateVerticalWalls()
+
+    --*******************************************************
+    --               ADD YOUR CODE HERE
+  
+    for i=-0.5,0.5,0.1
+    do
+     for j=0.45,-0.45,-0.1
+     do
+      wallobjectHandle=createWall()
+      sim.setObjectParent(wallobjectHandle,baseHandle,1)
+      sim.setObjectPosition(wallobjectHandle,sim.handle_parent,{i,j,wallHeight})
+      sim.setObjectOrientation(wallobjectHandle,sim.handle_parent,{0,0,1.5708})
+     end
+    end
+    
+    --*******************************************************
+end
+
+--[[
+**************************************************************
+	Function Name : deleteWalls()
+    Purpose:
+	---
+	Deletes all the walls in the given scene
+
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+    None
+	
+	Example call:
+	---
+	deleteWalls()
+**************************************************************	
+]]--
+function deleteWalls()
+
+    --*******************************************************
+    --               ADD YOUR CODE HERE
+    
+    for col = 0, 9, 1 do
+        for row = 0, 9, 1 do 
+            bin = decode(maze_array[row+1][col+1])
+            print(bin)
+            
+            if (bin[4] == 1) then
+                tmp2 = row+col*11
+                print("Cuboid"..tmp2)
+                handle_hori = sim.getObjectHandle("Cuboid"..tmp2)
+                sim.removeObject(handle_hori)
+            end
+            
+            if (bin[3] == 1) then
+                tmp = col*10+119+row
+                print("Cuboid"..tmp)
+                handle_verti = sim.getObjectHandle("Cuboid"..tmp)
+                sim.removeObject(handle_verti)
+            end            
+        end
+    end
+    
+    for i = 10, 98, 11 do
+        handle = sim.getObjectHandle("Cuboid"..i)
+        sim.removeObject(handle)
+    end
+    
+    for i = 109, 118, 1 do
+        handle = sim.getObjectHandle("Cuboid"..i)
+        sim.removeObject(handle)
+    end
+        
+    handle = sim.getObjectHandle("Cuboid")
+    sim.removeObject(handle)
+
+    maze_array = {}
+    --*******************************************************
+end
+
+
+--[[
+**************************************************************
+  YOU CAN DEFINE YOUR OWN INPUT AND OUTPUT PARAMETERS FOR THIS
+                          FUNCTION
+**************************************************************
+	Function Name : createMaze()
+    Purpose:
+	---
+	Creates the maze in the given scene by deleting specific 
+    horizontal and vertical walls
+
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+    None
+	
+	Example call:
+	---
+	createMaze()
+**************************************************************	
+]]--
+function createMaze()
+    
+    --*******************************************************
+    --               ADD YOUR CODE HERE
+    
+    for col = 0, 9, 1 do
+        for row = 0, 9, 1 do 
+            bin = decode(maze_array[row+1][col+1])
+            print(bin)
+            
+            if (bin[4] == 0) then
+                tmp2 = row+col*11
+                print("Cuboid"..tmp2)
+                handle_hori = sim.getObjectHandle("Cuboid"..tmp2)
+                sim.removeObject(handle_hori)
+            end
+            
+            if (bin[3] == 0) then
+                tmp = col*10+119+row
+                print("Cuboid"..tmp)
+                handle_verti = sim.getObjectHandle("Cuboid"..tmp)
+                sim.removeObject(handle_verti)
+            end            
+        end
+    end
+        
+    --*******************************************************
+end
+
+
+
+--[[
+**************************************************************
+	Function Name : sysCall_init()
+    Purpose:
+	---
+	Can be used for initialization of parameters
+    
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+    None
+	
+	Example call:
+	---
+	N/A
+**************************************************************	
+]]--
+function sysCall_init()
+
+    if pcall(saveTexture) then -- Do not delete or modify this section
+        print("Successfully saved texture")
+    else
+        print("Texture does not exist. Importing texture from file..")
+        retrieveTexture()
+        reapplyTexture()
+    end     
+end
+
+--[[
+**************************************************************
+        YOU ARE NOT ALLOWED TO MODIFY THIS FUNCTION. 
+**************************************************************
+	Function Name : sysCall_beforeSimulation()
+    Purpose:
+	---
+	This is executed before simulation starts
+    
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+    None
+	
+	Example call:
+	---
+	N/A
+**************************************************************	
+]]--
+function sysCall_beforeSimulation()
+    
+    sim.setShapeTexture(baseHandle, -1, sim.texturemap_plane, 0, {1.01, 1.01},nil,nil) -- Do not delete or modify this line
+    
+    generateHorizontalWalls()
+    generateVerticalWalls()
+    createMaze()
+end
+
+--[[
+**************************************************************
+        YOU ARE NOT ALLOWED TO MODIFY THIS FUNCTION. 
+**************************************************************
+	Function Name : sysCall_afterSimulation()
+    Purpose:
+	---
+	This is executed after simulation ends
+    
+	Input Arguments:
+	---
+	None
+	
+	Returns:
+	---
+    None
+	
+	Example call:
+	---
+	N/A
+**************************************************************	
+]]--
+function sysCall_afterSimulation()
+    -- is executed after a simulation ends
+    deleteWalls()
+    reapplyTexture() -- Do not delete or modify this line
+end
+
+function sysCall_cleanup()
+    -- do some clean-up here
+end
+
+-- See the user manual or the available code snippets for additional callback functions and details
+
+
+
+
